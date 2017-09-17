8ae4652d6e26c68f139402adbcbd959862fba214
VovanFun <smartselov@gmail.com>
2017-09-17 22:44:06 +0300

    initial

--- FootballSimulator/ViewController.swift
+++ FootballSimulator/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var viewAlert: UIView!
+    
+    @IBAction func AddTeam(_ sender: Any) {
+        viewAlert.isHidden = true
+        teams.append("")
+        tableView.reloadData()
+    }
+    
+    var teams = [String]()
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        viewAlert.isHidden = false
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = "\(teams[indexPath.row])"
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return teams.count-1
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    
 }
 
